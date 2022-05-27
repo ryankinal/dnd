117d70e668e4e4af816fe2b45349a132e01dba23
Ryan Kinal <ryan.kinal@gmail.com>
2022-05-27 13:04:48 -0400

    Added fate points to fate core

--- fate-dice/dice.js
+++ fate-dice/dice.js
@@ -5,12 +5,54 @@
 		diceIcons = document.querySelectorAll('.dice .fas'),
 		terribleDisplay = document.querySelector('tr.minus-2 td:first-child'),
 		legendaryDisplay = document.querySelector('tr.plus-8 td:first-child'),
+		fatePointsDisplay = document.querySelector('.fate-points'),
+		refreshValueInput = document.getElementById('refreshValue'),
+		stored = localStorage.getItem('fate'),
+		stored = stored ? JSON.parse(stored) : null,
+		fatePoints = 0,
 		rollAnimationCount = 8,
 		rollAnimationIntervalTime = 100,
-		iconClasses = [
+		diceIconClasses = [
 			'fas fa-minus',
 			'',
 			'fas fa-plus'
+		],
+		pointIconClasses = [
+		    "book-skull",
+		    "chess",
+		    "chess-bishop",
+		    "chess-board",
+		    "chess-king",
+		    "chess-knight",
+		    "chess-pawn",
+		    "chess-queen",
+		    "chess-rook",
+		    "diamond",
+		    "dice",
+		    "dice-d20",
+		    "dice-d6",
+		    "dice-five",
+		    "dice-four",
+		    "dice-one",
+		    "dice-six",
+		    "dice-three",
+		    "dice-two",
+		    "dragon",
+		    "dungeon",
+		    "gamepad",
+		    "ghost",
+		    "hand-fist",
+		    "hat-wizard",
+		    "headset",
+		    "heart",
+		    "puzzle-piece",
+		    "ring",
+		    "scroll",
+		    "shield-halved",
+		    "skull-crossbones",
+		    "square-full",
+		    "vr-cardboard",
+		    "wand-sparkles"
 		];
 
 	function fateDie() {
@@ -53,6 +95,30 @@
 		return sign + (total === 0 ? 'zero' : String(total));
 	}
 
+	function addFatePoint() {
+		var iconIndex = Math.floor(Math.random() * pointIconClasses.length),
+			icon = pointIconClasses[iconIndex],
+			element = document.createElement('div');
+
+		element.innerHTML = `<div class="fate-point"><div><span class="fas fa-${icon}"></span></div></div>`;
+		element.addEventListener('click', function() {
+			element.parentNode.removeChild(element);
+			fatePoints--;
+			save();
+		});
+
+		fatePoints++;
+		save();
+		fatePointsDisplay.appendChild(element);
+	}
+
+	function save() {
+		localStorage.setItem('fate', JSON.stringify({
+			fatePoints: fatePoints,
+			refreshValue: parseInt(refreshValueInput.value)
+		}))
+	}
+
 	rollButton.addEventListener('click', function() {
 		var rollAnimationInterval,
 			selected = document.querySelector('tr.selected');
@@ -71,32 +137,32 @@
 			legendaryDisplay.innerText = '+8';
 
 			rollAnimationInterval = setInterval(function() {
-				diceIcons[0].className = iconClasses[fateDie() + 1];
+				diceIcons[0].className = diceIconClasses [fateDie() + 1];
 				diceIcons[0].style.transform = 'rotate(' + randomRotation() + 'deg)';
 				
-				diceIcons[1].className = iconClasses[fateDie() + 1];
+				diceIcons[1].className = diceIconClasses [fateDie() + 1];
 				diceIcons[1].style.transform = 'rotate(' + randomRotation() + 'deg)';
 				
-				diceIcons[2].className = iconClasses[fateDie() + 1];
+				diceIcons[2].className = diceIconClasses [fateDie() + 1];
 				diceIcons[2].style.transform = 'rotate(' + randomRotation() + 'deg)';
 
-				diceIcons[3].className = iconClasses[fateDie() + 1];
+				diceIcons[3].className = diceIconClasses [fateDie() + 1];
 				diceIcons[3].style.transform = 'rotate(' + randomRotation() + 'deg)';
 			}, rollAnimationIntervalTime);
 
 			setTimeout(function() {
 				var result = roll();
 
-				diceIcons[0].className = iconClasses[result.dice[0] + 1];
+				diceIcons[0].className = diceIconClasses [result.dice[0] + 1];
 				diceIcons[0].style.transform = 'rotate(0deg)';
 				
-				diceIcons[1].className = iconClasses[result.dice[1] + 1];
+				diceIcons[1].className = diceIconClasses [result.dice[1] + 1];
 				diceIcons[1].style.transform = 'rotate(0deg)';
 				
-				diceIcons[2].className = iconClasses[result.dice[2] + 1];
+				diceIcons[2].className = diceIconClasses [result.dice[2] + 1];
 				diceIcons[2].style.transform = 'rotate(0deg)';
 
-				diceIcons[3].className = iconClasses[result.dice[3] + 1];
+				diceIcons[3].className = diceIconClasses [result.dice[3] + 1];
 				diceIcons[3].style.transform = 'rotate(0deg)';
 
 				clearInterval(rollAnimationInterval);
@@ -116,4 +182,33 @@
 			}, rollAnimationIntervalTime * rollAnimationCount);
 		}
 	});
+
+	document.getElementById('add').addEventListener('click', addFatePoint);
+
+	document.getElementById('refresh').addEventListener('click', function(e) {
+		var refreshValue = parseInt(refreshValueInput.value);
+
+		fatePointsDisplay.innerHTML = '';
+		fatePoints = 0;
+
+		while (refreshValue--) {
+			addFatePoint();
+		}
+	});
+
+	refreshValueInput.addEventListener('change', function() {
+		save();
+	});
+
+	if (stored) {
+		if (stored.fatePoints && stored.fatePoints > 0) {
+			while (stored.fatePoints--) {
+				addFatePoint();
+			}	
+		}
+
+		if (stored.refreshValue) {
+			refreshValueInput.value = stored.refreshValue;
+		}
+	}
 })();
